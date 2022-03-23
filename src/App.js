1b113b69ec600f8eed9f3db2f31cb942661fa6f9
陈辉 <chenhui@chenhuideMac-mini.local>
2022-03-23 18:28:38 +0800

    up

--- src/App.js
+++ src/App.js
@@ -1,24 +1,117 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect, useRef } from 'react';
+import { Table } from 'antd';
+import { filterData, insertItem } from './data-mock';
+import Builder from 'e-charts-builder-pro';
+
+const filter = {
+  issuerCodes: ["N000243"],
+}
 
 function App() {
+  const [list, setList] = useState(filterData);
+  const columns = [
+    {
+      title: '主体名称（发行人）',
+      dataIndex: 'issuerName',
+      key: 'issuerName',
+      width: '270px',
+    },
+  ];
+
+  useEffect(() => {
+    messageOn()
+  }, [])
+
+  // ----------------------------------------------------------------
+
+  const messageOn = () => {
+    console.log('messageOn');
+    console.log('list', list);
+    console.log('insertItem', insertItem);
+
+    // -------------------------------------------------------------------------
+    // -------------------------------------------------------------------------
+    // -------------------------------------------------------------------------
+
+    // 推送新成交
+    if (insertItem.messageType === 'DEAL_DEVIATION_CHANGE') {
+      let newList = list
+      let newItem = {
+        issuerName: insertItem.deviation.issuerName,
+        issuerCode: insertItem.deviation.issuerCode,
+      }
+
+      // 发行人过滤
+      if (filter.issuerCodes.length > 0) {
+        if (filter.issuerCodes.indexOf(insertItem.deviation.issuerCode) !== -1) {
+          newList = list.filter(item => item.issuerCode !== insertItem.deviation.issuerCode);
+        }
+        else {
+          newItem = []
+        };
+      } else {
+        if (list.some(item => item.issuerCode === insertItem.deviation.issuerCode)) {
+          newList = list.filter(item => item.issuerCode !== insertItem.deviation.issuerCode);
+        }
+      }
+
+
+
+
+      // ---------------------------------------------------------------
+      // ...
+
+      // ---------------------------------------------------------------
+      // 更新 list
+      // console.log('newList', newList, newItem);
+      setList([...newList, newItem]);
+    }
+
+    // -------------------------------------------------------------------------
+    // -------------------------------------------------------------------------
+    // -------------------------------------------------------------------------
+
+    // 推送移出成交
+    if (insertItem.messageType === 'DEAL_DEVIATION_REVOKE') {
+      // ...
+    }
+  }
+
+
+
+  // ----------------------------------------------------------------
+
+  const ref = useRef(null)
+
+
+  if (ref.current) {
+    const myChart = new Builder({ container: ref.current })
+
+    const option = {
+      xAxis: {
+        type: 'category',
+        data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+      },
+      yAxis: {
+        type: 'value'
+      },
+      series: [
+        {
+          data: [150, 230, 224, 218, 135, 147, 260],
+          type: 'line'
+        }
+      ]
+    };
+
+    myChart && myChart.setOption(option).xAxis(option.xAxis).yAxis(option.yAxis).series(option.series).render();
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Table rowKey="issuerCode" columns={columns} dataSource={list} />
+      <div ref={ref} style={{ width: 400, height: 300 }}></div>
+    </>
+
   );
 }
 
---
+++ src/data-mock.js
@@ -0,0 +1,440 @@
+const data = [
+    {
+        "issuerName": "南京江宁城市建设集团有限公司",
+        "issuerCode": "N000243",
+        "issuerRate": "AAA",
+        "bondCount": 0,
+        "dealDay": 4,
+        "lastDealDate": "2022-03-21",
+        "brokerDealCount": 1,
+        "brokerDealBondCount": 1,
+        "brokerDealBondCountList": [
+            {
+                "bondId": "102100459.IB",
+                "count": 1
+            }
+        ],
+        "brokerHighDealCount": 0,
+        "brokerHighDealBondCount": 0,
+        "brokerHighDealBondCountList": [
+
+        ],
+        "brokerLowDealCount": 1,
+        "brokerLowDealBondCount": 1,
+        "brokerLowDealBondCountList": [
+            {
+                "bondId": "102100459.IB",
+                "count": 1
+            }
+        ],
+        "brokerFirstDeal": "--",
+        "cfetsDealBondCount": 0,
+        "cfetsDealBondCountList": [
+
+        ],
+        "cfetsHighBondCount": 0,
+        "cfetsHighBondCountList": [
+
+        ],
+        "cfetsLowBondCount": 0,
+        "cfetsLowBondCountList": [
+
+        ],
+        "cfetsFirstDeal": "--",
+        "ssetDealBondCount": 0,
+        "ssetDealBondCountList": [
+
+        ],
+        "ssetHighBondCount": 0,
+        "ssetHighBondCountList": [
+
+        ],
+        "ssetLowBondCount": 0,
+        "ssetLowBondCountList": [
+
+        ],
+        "ssetFirstDeal": "--",
+        "industry": null,
+        "institutionSubType": null,
+        "area": "江苏",
+        "issuerScore": "--/--",
+        "issuerCreditSpread": null
+    },
+    {
+        "issuerName": "绍兴市柯桥区城建投资开发集团有限公司",
+        "issuerCode": "S000937",
+        "issuerRate": "AA",
+        "bondCount": 0,
+        "dealDay": 3,
+        "lastDealDate": "2022-03-17",
+        "brokerDealCount": 1,
+        "brokerDealBondCount": 1,
+        "brokerDealBondCountList": [
+            {
+                "bondId": "032191222.IB",
+                "count": 1
+            }
+        ],
+        "brokerHighDealCount": 0,
+        "brokerHighDealBondCount": 0,
+        "brokerHighDealBondCountList": [
+
+        ],
+        "brokerLowDealCount": 0,
+        "brokerLowDealBondCount": 0,
+        "brokerLowDealBondCountList": [
+
+        ],
+        "brokerFirstDeal": "--",
+        "cfetsDealBondCount": 0,
+        "cfetsDealBondCountList": [
+
+        ],
+        "cfetsHighBondCount": 0,
+        "cfetsHighBondCountList": [
+
+        ],
+        "cfetsLowBondCount": 0,
+        "cfetsLowBondCountList": [
+
+        ],
+        "cfetsFirstDeal": "--",
+        "ssetDealBondCount": 0,
+        "ssetDealBondCountList": [
+
+        ],
+        "ssetHighBondCount": 0,
+        "ssetHighBondCountList": [
+
+        ],
+        "ssetLowBondCount": 0,
+        "ssetLowBondCountList": [
+
+        ],
+        "ssetFirstDeal": "--",
+        "industry": null,
+        "institutionSubType": null,
+        "area": "浙江",
+        "issuerScore": "--/--",
+        "issuerCreditSpread": null
+    },
+    {
+        "issuerName": "中国中信有限公司",
+        "issuerCode": "Z000460",
+        "issuerRate": "AAA",
+        "bondCount": 0,
+        "dealDay": 1,
+        "lastDealDate": "2022-03-18",
+        "brokerDealCount": 1,
+        "brokerDealBondCount": 1,
+        "brokerDealBondCountList": [
+            {
+                "bondId": "188944.SH",
+                "count": 1
+            }
+        ],
+        "brokerHighDealCount": 0,
+        "brokerHighDealBondCount": 0,
+        "brokerHighDealBondCountList": [
+
+        ],
+        "brokerLowDealCount": 1,
+        "brokerLowDealBondCount": 1,
+        "brokerLowDealBondCountList": [
+            {
+                "bondId": "188944.SH",
+                "count": 1
+            }
+        ],
+        "brokerFirstDeal": "--",
+        "cfetsDealBondCount": 0,
+        "cfetsDealBondCountList": [
+
+        ],
+        "cfetsHighBondCount": 0,
+        "cfetsHighBondCountList": [
+
+        ],
+        "cfetsLowBondCount": 0,
+        "cfetsLowBondCountList": [
+
+        ],
+        "cfetsFirstDeal": "--",
+        "ssetDealBondCount": 0,
+        "ssetDealBondCountList": [
+
+        ],
+        "ssetHighBondCount": 0,
+        "ssetHighBondCountList": [
+
+        ],
+        "ssetLowBondCount": 0,
+        "ssetLowBondCountList": [
+
+        ],
+        "ssetFirstDeal": "--",
+        "industry": null,
+        "institutionSubType": null,
+        "area": "北京",
+        "issuerScore": "--/--",
+        "issuerCreditSpread": null
+    },
+    {
+        "issuerName": "镇江交通产业集团有限公司",
+        "issuerCode": "Z000066",
+        "issuerRate": "A-",
+        "bondCount": 0,
+        "dealDay": 5,
+        "lastDealDate": "2022-03-18",
+        "brokerDealCount": 1,
+        "brokerDealBondCount": 1,
+        "brokerDealBondCountList": [
+            {
+                "bondId": "012103854.IB",
+                "count": 1
+            }
+        ],
+        "brokerHighDealCount": 0,
+        "brokerHighDealBondCount": 0,
+        "brokerHighDealBondCountList": [
+
+        ],
+        "brokerLowDealCount": 1,
+        "brokerLowDealBondCount": 1,
+        "brokerLowDealBondCountList": [
+            {
+                "bondId": "012103854.IB",
+                "count": 1
+            }
+        ],
+        "brokerFirstDeal": "--",
+        "cfetsDealBondCount": 0,
+        "cfetsDealBondCountList": [
+
+        ],
+        "cfetsHighBondCount": 0,
+        "cfetsHighBondCountList": [
+
+        ],
+        "cfetsLowBondCount": 0,
+        "cfetsLowBondCountList": [
+
+        ],
+        "cfetsFirstDeal": "--",
+        "ssetDealBondCount": 0,
+        "ssetDealBondCountList": [
+
+        ],
+        "ssetHighBondCount": 0,
+        "ssetHighBondCountList": [
+
+        ],
+        "ssetLowBondCount": 0,
+        "ssetLowBondCountList": [
+
+        ],
+        "ssetFirstDeal": "--",
+        "industry": null,
+        "institutionSubType": null,
+        "area": "江苏",
+        "issuerScore": "--/--",
+        "issuerCreditSpread": null
+    },
+    {
+        "issuerName": "昆山城市建设投资发展集团有限公司",
+        "issuerCode": "K000083",
+        "issuerRate": "AAA",
+        "bondCount": 0,
+        "dealDay": 3,
+        "lastDealDate": "2022-03-18",
+        "brokerDealCount": 1,
+        "brokerDealBondCount": 1,
+        "brokerDealBondCountList": [
+            {
+                "bondId": "012103503.IB",
+                "count": 1
+            }
+        ],
+        "brokerHighDealCount": 0,
+        "brokerHighDealBondCount": 0,
+        "brokerHighDealBondCountList": [
+
+        ],
+        "brokerLowDealCount": 0,
+        "brokerLowDealBondCount": 0,
+        "brokerLowDealBondCountList": [
+
+        ],
+        "brokerFirstDeal": "--",
+        "cfetsDealBondCount": 0,
+        "cfetsDealBondCountList": [
+
+        ],
+        "cfetsHighBondCount": 0,
+        "cfetsHighBondCountList": [
+
+        ],
+        "cfetsLowBondCount": 0,
+        "cfetsLowBondCountList": [
+
+        ],
+        "cfetsFirstDeal": "--",
+        "ssetDealBondCount": 0,
+        "ssetDealBondCountList": [
+
+        ],
+        "ssetHighBondCount": 0,
+        "ssetHighBondCountList": [
+
+        ],
+        "ssetLowBondCount": 0,
+        "ssetLowBondCountList": [
+
+        ],
+        "ssetFirstDeal": "--",
+        "industry": null,
+        "institutionSubType": null,
+        "area": "江苏",
+        "issuerScore": "--/--",
+        "issuerCreditSpread": null
+    },
+    {
+        "issuerName": "随州市城市投资集团有限公司",
+        "issuerCode": "S000269",
+        "issuerRate": "A-",
+        "bondCount": 0,
+        "dealDay": 2,
+        "lastDealDate": "2022-03-21",
+        "brokerDealCount": 2,
+        "brokerDealBondCount": 1,
+        "brokerDealBondCountList": [
+            {
+                "bondId": "012280200.IB",
+                "count": 2
+            }
+        ],
+        "brokerHighDealCount": 0,
+        "brokerHighDealBondCount": 0,
+        "brokerHighDealBondCountList": [
+
+        ],
+        "brokerLowDealCount": 2,
+        "brokerLowDealBondCount": 1,
+        "brokerLowDealBondCountList": [
+            {
+                "bondId": "012280200.IB",
+                "count": 2
+            }
+        ],
+        "brokerFirstDeal": "--",
+        "cfetsDealBondCount": 0,
+        "cfetsDealBondCountList": [
+
+        ],
+        "cfetsHighBondCount": 0,
+        "cfetsHighBondCountList": [
+
+        ],
+        "cfetsLowBondCount": 0,
+        "cfetsLowBondCountList": [
+
+        ],
+        "cfetsFirstDeal": "--",
+        "ssetDealBondCount": 0,
+        "ssetDealBondCountList": [
+
+        ],
+        "ssetHighBondCount": 0,
+        "ssetHighBondCountList": [
+
+        ],
+        "ssetLowBondCount": 0,
+        "ssetLowBondCountList": [
+
+        ],
+        "ssetFirstDeal": "--",
+        "industry": null,
+        "institutionSubType": null,
+        "area": "湖北",
+        "issuerScore": "--/--",
+        "issuerCreditSpread": null
+    },
+]
+
+const filterData = data.map(item => {
+    return {
+        issuerName: item.issuerName,
+        issuerCode: item.issuerCode,
+    }
+})
+
+const insertItem = {
+    "firstFilterkey": "*",
+    "secondFilterkey": "*",
+    "deviation": {
+        "executionId": "TPSH220323011014000000000289",
+        "contributorId": "国利",
+        "platform": 1,
+        "bondName": "21华电SCP022",
+        "bondId": "012103766.IB",
+        "bondKey": "Z0001562021CORCSP22",
+        "listedMarket": "CIB",
+        "restMaturity": "114D",
+        "restMaturityFirst": 0.31232876712328766,
+        "restMaturityFirstType": "1",
+        "cdcDiffReason": "2",
+        "csiDiffReason": "2",
+        "dealCleanPrice": null,
+        "cdcSmallOrder": false,
+        "csiSmallOrder": false,
+        "cdcDiffBP": -0.05,
+        "cdcCleanPxDiffBP": -0.0006000000000000001,
+        "csiDiffBP": -1.17,
+        "csiCleanPxDiffBP": 0.010400000000000001,
+        "highDiffIsCdc": false,
+        "yield": 2.54,
+        "tradeDate": "2022-03-23",
+        "tradeTime": "2022-03-23T13:35:10",
+        "hasOption": false,
+        "perpetual": false,
+        "municipal": false,
+        "couponRate": 2.65,
+        "issueRating": "AAA",
+        "bondRating": "--",
+        "productType": 20609,
+        "issuerName": "测试主体名称 - 替换",
+        // "issuerCode": "Z000156",
+        "issuerCode": "N000243",
+        "impliedRating": "--",
+        "impliedRatingType": "OTH",
+        "industry": "产业",
+        "swSector20212": "电力",
+        "institutionSubType": "央企",
+        "issueMode": "BPB",
+        "cdcExerciseFlag": false,
+        "csiExerciseFlag": false,
+        "scoreLevel": null,
+        "score": null,
+        "creditSpread": null,
+        "dealStatus": 0,
+        "cdcShortDur": true,
+        "csiShortDur": true,
+        "cdcValuation": 2.5405,
+        "csiValuation": 2.5517,
+        "marketCredit": true,
+        "ncd": false,
+        "modifiedDuration": "0.3071",
+        "issuerScoreLevel": null,
+        "issuerScore": null,
+        "issuerCreditSpread": null,
+        "area": null,
+        "swSector20211": "公用事业",
+        "isListedType": "--",
+        "commonType": "公用事业,电力,--"
+    },
+    "executionId": null,
+    "messageType": "DEAL_DEVIATION_CHANGE"
+};
+
+
+export { filterData, insertItem }
